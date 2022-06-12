ae499bbd71aa6a5c321d7c75e1261850a0741838
John Patrick Resurreccion <patrickjohn.resurreccion18@gmail.com>
2022-06-12 13:01:53 +0800

    modify some routes

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Layout from "../components/admin/Layout";
 
 function MyApp({ Component, pageProps }) {
   const baseUri = "https://oic-management.herokuapp.com";
+  // const baseUri = "http://localhost:5000";
   return (
     <Layout>
       <BaseUri.Provider value={baseUri}>
