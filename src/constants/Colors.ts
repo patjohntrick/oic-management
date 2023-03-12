b021903b8ac165d938b59af0d3736ccff2b49efe
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-12 10:51:52 +0800

    migration: appColors

---
+++ src/constants/Colors.ts
@@ -0,0 +1,16 @@
+export const appColors = {
+  white: '#fff',
+  ghostWhite: '#f5f6fb',
+
+  black: '#000',
+  oliveBlack: '#3d3d3d',
+  black10: 'rgba(0, 0, 0, .10)',
+
+  majorelleBlue: '#713bdb',
+  ultraMarineBlue: '#3a76ef',
+  mayaBlue: '#63c6ff',
+
+  richLavenderViolet: '#a66dd4',
+
+  mediumAquaMarineGreen: '#6ed3b2',
+};
