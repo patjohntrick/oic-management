d05d772224c0a1b65b84a65e13a191c7ddf50266
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-12 10:51:36 +0800

    migration: working on strings constant

---
+++ src/constants/Strings.ts
@@ -0,0 +1,31 @@
+export const Strings = {
+  LOGO: 'OIC Inc.',
+  ADD_MEMBER: 'Add Member',
+};
+
+export const LINKS = [
+  {
+    text: 'Dashboard',
+    linkTo: '/',
+  },
+  {
+    text: 'Members',
+    linkTo: '/members',
+  },
+  {
+    text: 'Donations',
+    linkTo: '/donations',
+  },
+  {
+    text: 'Activities',
+    linkTo: '/activities',
+  },
+  {
+    text: 'Storage',
+    linkTo: '/storage',
+  },
+  {
+    text: 'Logs',
+    linkTo: '/logs',
+  },
+];
