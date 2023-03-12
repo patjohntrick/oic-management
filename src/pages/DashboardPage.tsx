bc5d4233ae7c6d2221ef731e1eada3d2c8ccdd73
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-12 10:50:33 +0800

    migration: start working on dashboard page

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Box, Button, Typography } from '@mui/material';
+import { Layout } from '../components';
+
+export const DashboardPage = () => {
+  return (
+    <Layout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+        necessitatibus itaque maiores pariatur ipsam minima illum saepe quam vel
+        amet.
+      </Typography>
+    </Layout>
+  );
+};
