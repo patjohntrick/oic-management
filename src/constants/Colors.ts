0612a2c793efac7817afb43a34f91ba1224abbcf
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-15 10:53:55 +0800

    migration: add colors

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,11 +1,14 @@
 export const appColors = {
+  // white
   white: '#fff',
   ghostWhite: '#f5f6fb',
 
+  // black
   black: '#000',
   oliveBlack: '#3d3d3d',
   black10: 'rgba(0, 0, 0, .10)',
 
+  // blue
   majorelleBlue: '#713bdb',
   majorelleBlue10: '#713bdb1a',
   ultraMarineBlue: '#3a76ef',
@@ -13,15 +16,26 @@ export const appColors = {
   crayolaBlue: '#2563eb',
   crayolaBlue10: '#2563eb1a',
 
+  // violet
   richLavenderViolet: '#a66dd4',
 
+  // green
   mediumAquaMarineGreen: '#6ed3b2',
   pigmentGreen: '#16a34a',
   pigmentGreen10: '#16a34a1a',
+  nyanzaGreen: '#dcfce7',
 
+  // red
   maximumRed: '#dc2626',
-  maximumRed10: '#dc26261a',
+  mistyRoseRed: '#fee2e2',
 
+  // gold
   chineseGold: '#ca8a04',
   chineseGold10: '#ca8a041a',
+
+  // yellow
+  veryPaleYellow: ' #fef9c3',
+
+  // cyan
+  lightCyan: '#e0f2fe',
 };
