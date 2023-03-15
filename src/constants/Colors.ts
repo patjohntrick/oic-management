36ce18a0c5ac4bc0e9effbfb4e9fcadecd940bc8
patjohntrick <patrickjohn.resurreccion18@gmail.com>
2023-03-15 10:45:33 +0800

    migration: add colors

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -7,10 +7,21 @@ export const appColors = {
   black10: 'rgba(0, 0, 0, .10)',
 
   majorelleBlue: '#713bdb',
+  majorelleBlue10: '#713bdb1a',
   ultraMarineBlue: '#3a76ef',
   mayaBlue: '#63c6ff',
+  crayolaBlue: '#2563eb',
+  crayolaBlue10: '#2563eb1a',
 
   richLavenderViolet: '#a66dd4',
 
   mediumAquaMarineGreen: '#6ed3b2',
+  pigmentGreen: '#16a34a',
+  pigmentGreen10: '#16a34a1a',
+
+  maximumRed: '#dc2626',
+  maximumRed10: '#dc26261a',
+
+  chineseGold: '#ca8a04',
+  chineseGold10: '#ca8a041a',
 };
